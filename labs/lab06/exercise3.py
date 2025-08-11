96059234beeb3338afbe486f23733ff44a763d64
bsyiaabalqis <batrisyiaaa.balqiss@gmail.com>
2025-08-11 12:03:26 +0800

    lab 6

---
+++ labs/lab06/exercise3.py
@@ -0,0 +1,8 @@
+full_name=  'Ali Rahman'
+print(full_name)
+uppercase_name= "ALI RAHMAN"
+print(uppercase_name) 
+lowercase_name= 'Ali rahman'
+print(lowercase_name)
+name_length= 10
+print(name_length)
---
+++ labs/lab06/exercise4.py
@@ -0,0 +1,3 @@
+print("RECEIPT")
+print("\n\nItem\tPrice\tQty\tTotal")
+print("\n\nCoffee\t$3.50\t2\t$7.00")
