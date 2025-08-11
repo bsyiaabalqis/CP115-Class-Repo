a4f74be70ca14b2f4f69d38fc2c178e15c6e78bf
bsyiaabalqis <batrisyiaaa.balqiss@gmail.com>
2025-08-11 12:17:03 +0800

    exercise 2

--- labs/lab06/exercise2.py
+++ labs/lab06/exercise2.py
@@ -1,10 +1,10 @@
 # If your string contains single quotes, use double quotes
-message1 = "You're in  the hospital area"
+message1 = "You're in the hospital area"
 print(message1)
 
 # If your string contains double quotes, use single quotes
-message2 = 'Please wear your facemask'
+message2 = '"Please wear your facemask"'
 print(message2)
 
-message3 = 'He said "Thank you'
+message3 = 'He said "Thank you"'
 print(message3)
