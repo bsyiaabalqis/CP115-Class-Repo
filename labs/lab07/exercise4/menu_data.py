865364ac2d896fe5372859b99319c6504e6d1a68
bsyiaabalqis <batrisyiaaa.balqiss@gmail.com>
2025-08-21 01:59:29 +0800

    exercise 3

--- labs/lab07/exercise4/menu_data.py
+++ labs/lab07/exercise4/menu_data.py
@@ -6,5 +6,3 @@ Restaurant_Name = "The Coding Cafe"
 Restaurant_location = "123 Code St, Tech City"
 Menu_Items = ["Coffee", "Bread", "MilkShake"]
 
-print(f"Restaurant_Name in UPPERCASE: {my.name_upper}")
-print(f"Name length: {cafe.name_length} characters")
