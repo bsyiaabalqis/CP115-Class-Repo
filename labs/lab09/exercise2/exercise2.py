0af90f0c6f1a6ba1043269162aa6d630a2e503d7
bsyiaabalqis <batrisyiaaa.balqiss@gmail.com>
2025-09-08 13:15:04 +0800

    lab 9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -4,7 +4,16 @@ overtime_hours = int(input())
 tax_status = input()
 
 # TODO your code here
-
+if base_salary >= 5000 and "Single" 
+    if tax_status == "single":
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+elif base_salary >= 6000:
+    if tax_status == "married":
+        tax_rate = 0.20  
+    else: 
+        tax_rate = 0.15
 
 print(employee_name)
 print(tax_rate)
