3022fe7f5ac438f37b4b33b4b0713939d0fbd166
bsyiaabalqis <batrisyiaaa.balqiss@gmail.com>
2025-10-27 13:08:49 +0800

    lab 11

--- labs/lab11/exercise3/exercise3.py
+++ labs/lab11/exercise3/exercise3.py
@@ -1,21 +1,14 @@
 target_points = input("Enter target points:")
 total = 0 
 rounds_played = 0 
-total_points = 0
 
 # TODO: Your code here
 # Use input() inside the while loop to get points each round
 # Custom step pattern 
-while True: 
-    target_points = int(input("Enter target points:"))
-    points = int(input("Enter points:"))
+while total< target_points:
+    points = float(input("Enter points:"))
     total+= points
     rounds_played += 1
-    total_points = total
 
-    if total>= target_points:
-        points = float(input())
-        break
-
-print(total_points)
+print(total)
 print(rounds_played)
